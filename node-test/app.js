17c756b4993e62c7405e2a3aa418ca48f5edbaf4
Tony W <bobacrypto2@gmail.com>
2023-08-05 03:40:34 -0700

    appendFile in fs

--- node-test/app.js
+++ node-test/app.js
@@ -5,7 +5,7 @@ const fs = require("fs/promises");
 async function example() {
   try {
     const content = "Some promise content!";
-    await fs.writeFile("./fs-test-rename.txt", content);
+    await fs.appendFile("./fs-test-rename.txt", content);
   } catch (err) {
     console.log(err);
   }
