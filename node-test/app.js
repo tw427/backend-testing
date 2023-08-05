ab700a09499e588e5134895299a4112a21cc8526
Tony W <bobacrypto2@gmail.com>
2023-08-05 03:18:53 -0700

    Example of a GET request using Node.js standard modules

--- node-test/app.js
+++ node-test/app.js
@@ -1,3 +1,24 @@
 #!/usr/bin/env node
 
-console.log("Hello World");
+const https = require("https");
+
+const options = {
+  hostname: "example.com",
+  port: 443,
+  path: "todos",
+  method: "GET",
+};
+
+const req = https.request(options, (res) => {
+  console.log(`statusCode: ${res.statusCode}`);
+
+  res.on("data", (d) => {
+    process.stdout.write(d);
+  });
+});
+
+req.on("error", (error) => {
+  console.log(error);
+});
+
+req.end();
