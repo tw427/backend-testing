6801ed6ba9d2b7b42a97ee5f89faddfdbdfc9bd1
Tony W <bobacrypto2@gmail.com>
2023-08-13 23:58:41 -0700

    Install express and rewrite Hello World in Node with Express

--- node-test/app.js
+++ node-test/app.js
@@ -1,18 +1,11 @@
-const http = require("http");
+const express = require("express");
+const app = express();
+const port = 3000;
 
-const hostname = "127.0.0.1";
-const port = 8000;
-
-// Create HTTP server
-const server = http.createServer(function (req, res) {
-  // Set the response HTTP header with HTTP status and Content type
-  res.writeHead(200, { "Content-Type": "text/plain" });
-
-  // Send the response body "Hello World"
-  res.end("Hello World\n");
+app.get("/", function (req, res) {
+  res.send("Hello World!");
 });
 
-// Prints a log once the server starts listening
-server.listen(port, hostname, function () {
-  console.log(`Sever running at http://${hostname}:${port}`);
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}`);
 });
