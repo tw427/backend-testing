3721612ed0fe7575573894565f117c6367d1e292
Tony W <bobacrypto2@gmail.com>
2023-09-08 17:24:39 -0700

    Write HTTP methods to retrieve data from our Express app

--- node-test/app.js
+++ node-test/app.js
@@ -2,20 +2,56 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.send("Received a GET HTTP METHOD");
+let users = {
+  1: {
+    id: "1",
+    username: "Robin Wieruch",
+  },
+  2: {
+    id: "2",
+    username: "Dave Davids",
+  },
+};
+
+let messages = {
+  1: {
+    id: "1",
+    text: "Hello World",
+    userId: "1",
+  },
+  2: {
+    id: "2",
+    text: "By World",
+    userId: "2",
+  },
+};
+
+app.get("/users", (req, res) => {
+  return res.send(Object.values(users));
 });
 
-app.put("/", (req, res) => {
-  return res.send("Received a PUT HTTP METHOD");
+app.get("/users/:userId", (req, res) => {
+  return res.send(users[req.params.userId]);
 });
 
-app.post("/", (req, res) => {
+app.get("/messages", (req, res) => {
+  return res.send(Object.values(messages));
+});
+
+app.get("/messages/:messageId", (req, res) => {
+  return res.send(messages[req.params.messageId]);
+});
+
+app.post("/users", (req, res) => {
   return res.send("Received a POST HTTP METHOD");
 });
 
-app.delete("/", (req, res) => {
-  return res.send("Received a DELETE HTTP METHOD");
+app.put("/users/:userId", (req, res) => {
+  return res.send(`POST HTTP method on user: ${req.params.userId}`);
+});
+
+app.delete("/users/:userId", (req, res) => {
+  return res.send(`DELETE HTTP method on user: ${req.params.userId}`);
 });
 
 app.listen(3000, () => console.log("server started"));
