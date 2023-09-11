b0a93a6c81a06cc0e4847a5ce7c57cc45d0e175c
Tony W <bobacrypto2@gmail.com>
2023-09-10 17:14:16 -0700

    Create verifyToken function taking the access token from our header

--- node-test/app.js
+++ node-test/app.js
@@ -1,22 +1,71 @@
 const express = require("express");
-const models = require("../models");
-const routes = require("../routes");
+const jwt = require("jsonwebtoken");
+// const models = require("../models");
+// const routes = require("../routes");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-  req.context = {
-    models,
-    me: models.users[1],
-  };
-  next();
-});
-app.use("/users", routes.user);
-app.use("/messages", routes.message);
-app.use("/session", routes.session);
 
-app.listen(3000, () => console.log("server started"));
+app.get("/api", (req, res) => {
+  res.json({ message: "Welcome to the API" });
+});
+
+app.post("/api/posts", verifyToken, (req, res) => {
+  res.json({
+    message: "Post created...",
+  });
+});
+
+app.post("/api/login", (req, res) => {
+  // Mock user
+  const user = {
+    id: 1,
+    username: "Odin",
+    email: "odin@gmail.com",
+  };
+
+  jwt.sign({ user: user }, "odin", (err, token) => {
+    res.json({
+      token: token,
+    });
+  });
+});
+
+// FORMAT OF TOKEN
+// Authorization: Bearer <access_token>
+
+// Verify Token
+function verifyToken(req, res, next) {
+  // Get auth header value
+  const bearerHeader = req.headers["authorization"];
+  // Check if bearer is undefined
+  if (typeof bearerHeader !== "undefined") {
+    // Split Bearer <access_token> at the space
+    const bearer = bearerHeader.split(" ");
+    // Get token from array ^
+    const bearerToken = bearer[1];
+    // Set the token
+    req.token = bearerToken;
+    next();
+  } else {
+    // Forbidden
+    res.sendStatus(403);
+  }
+}
+
+// app.use((req, res, next) => {
+//   req.context = {
+//     models,
+//     me: models.users[1],
+//   };
+//   next();
+// });
+// app.use("/users", routes.user);
+// app.use("/messages", routes.message);
+// app.use("/session", routes.session);
+
+app.listen(3000, () => console.log("Server started on 3000"));
 
 module.exports = app;
